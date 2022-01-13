4255ee7d745e963e98ad00b67003562438b4e38c
seanstory <skyloftproductions@Skylofts-iMac.hitronhub.home>
2022-01-12 23:24:42 -0800

    changed homepage url again

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import FeatherIcon from "feather-icons-react";
 const Header = () => {
   return (
     <nav className="navbar">
-      <Link className="link" to='./home'><div> <FeatherIcon icon='home' className='homeicon'/></div></Link>
+      <Link className="link" to='./'><div> <FeatherIcon icon='home' className='homeicon'/></div></Link>
       <ul className="navlinks">
         <Link className="link" to="./projects">Projects</Link>
         <Link className="link" to="./about">About</Link>
