6db9fd62a6b4275f71e4cf1006db1f8cde8a1c20
seanstory <skyloftproductions@Skylofts-iMac.hitronhub.home>
2022-01-12 23:00:42 -0800

    changed homepage url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Header/>
 
       <Routes> 
-        <Route path='home' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='projects' element={<Projects/>}/>
         <Route path='about' element={<About/>}/>
         <Route path='contact' element={<Contact/>}/>
