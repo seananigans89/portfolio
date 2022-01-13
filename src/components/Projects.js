14557c4bd7b1a952e189da1088cf520a94049994
seanstory <skyloftproductions@Skylofts-iMac.hitronhub.home>
2022-01-13 11:50:03 -0800

    image styling for projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,18 +8,18 @@ const Projects = () => {
       id: 1,
       header: "Overview",
       description:
-        "Sorted is an inventory management app specifically designed for keeping track of photo/video equipment.",
+        "Sorted is an inventory management app specifically designed for keeping track of photo/video equipment. The user can input all of their gear with specific categories for easy sorting, and create multi-category kits to be shared with other users, facilitating a peer-to-peer equipment rental ecosystem.",
       image: "https://i.imgur.com/Qy0Ae4V.png",
     },
     {
       id: 2,
       header: "Lessons",
-      description: "I learned a great deal building my first React Native App.",
+      description: "This was my first React Native project and I really enjoyed the experience. I think the best lesson from this build came in the area of UX design. The structured nature of React Native and the incredible open source support meant that there is almost always a component that will do what you need, it's just a matter of properly integrating it into the build.",
       image: "https://i.imgur.com/Qy0Ae4V.png",
     },
     {
       id: 3,
-      header: "More Info",
+      header: "Next Step",
       description:
         "The next feature I would like to add is native camera functionality to enable barcode and QR code scanning.",
       image: "https://i.imgur.com/Qy0Ae4V.png",
@@ -36,14 +36,14 @@ const Projects = () => {
     {
       id: 2,
       header: "Lessons",
-      description: "I learned a great deal building my first React Native App.",
+      description: "This was my first React project and I had a blast building it. One of the biggest hurdles was being able to randomly select 8 Pokemon from 151 before duplicating each one and shuffling them to form the grid. This project helped solidify my understanding of State in React.",
       image: "https://i.imgur.com/ozqwyMd.png",
     },
     {
       id: 3,
-      header: "More Info",
+      header: "Next Step",
       description:
-        "The next feature I would like to add is native camera functionality to enable barcode and QR code scanning.",
+        "I'm working on adding functionality to change the grid size for a more challenging game, as well as selecting a specific generation of Pokemon.",
       image: "https://i.imgur.com/ozqwyMd.png",
     },
   ];
@@ -51,12 +51,13 @@ const Projects = () => {
     <div className="cardcontent">
       <div className="cardblock1">
         <h1 className="projecttitle">Sorted</h1>
-        <CardView
+        <div className="proj1"><CardView
           items={sorted}
           activeColor="#000"
           imageHeight="350px"
           imageWidth="175px"
-        />
+        /></div>
+        
       </div>
       <div className="cardblock2">
         <h1 className="projecttitle">Pokématch</h1>
